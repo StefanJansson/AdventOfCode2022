469adc55ef75fe2c1feab6f1a6843a3dcaa22061
Stefan Jansson <stefan@veum.se>
2022-12-02 01:44:34 +0100

    Add project files.

---
+++ AokSJ/DailyQuiz.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AokSJ
+{
+    public class DailyQuiz
+    {
+        public static List<object> Day01()
+        {
+            var fileData = System.IO.File.ReadAllText(@"..\..\..\Files\2022-12-01.txt");
+            var splitFileData = fileData.Split("\n\n");
+            var summaries = splitFileData
+                .Select(_ => Regex.Matches(_, @"\d+").Select(_ => Convert.ToInt64(_.Value)).Sum())
+                .OrderByDescending(_ => _).ToList();
+            return new List<object>() { "2022-12-01", summaries.Max(), summaries.Take(3).Sum()};
+        }
+    }
+}
---
+++ AokSJ/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using AokSJ;
+using ConsoleTableExt;
+
+Console.WriteLine("AdventOfCode!");
+Console.WriteLine("");
+
+var columnNames = new List<string>() { "Date", "Part One", "Part Two" };
+var summaries = new List<List<object>>() { DailyQuiz.Day01()};
+var alignments = new Dictionary<int, TextAligntment>
+{
+    {1, TextAligntment.Right},
+    {2, TextAligntment.Right}
+};
+ConsoleTableBuilder
+    .From(summaries)
+    .WithColumn(columnNames)
+    .WithFormat(ConsoleTableBuilderFormat.Minimal)
+    .WithTextAlignment(alignments)
+
+    //.WithOptions(new ConsoleTableBuilderOption { DividerString = "" })
+    .ExportAndWriteLine();
