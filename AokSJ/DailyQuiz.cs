2d058bccec257276e694f00e85a515b4cd147060
Stefan Jansson <stefan@veum.se>
2022-12-02 02:11:39 +0100

    Snyggat till lite :)

--- AokSJ/DailyQuiz.cs
+++ AokSJ/DailyQuiz.cs
@@ -13,5 +13,121 @@ namespace AokSJ
                 .OrderByDescending(_ => _).ToList();
             return new List<object>() { "2022-12-01", summaries.Max(), summaries.Take(3).Sum()};
         }
+
+        public static List<object> Day02()
+        {
+            return new List<object>() { "2022-12-02", "", "" };
+        }
+
+        public static List<object> Day03()
+        {
+            return new List<object>() { "2022-12-03", "", "" };
+        }
+
+        public static List<object> Day04()
+        {
+            return new List<object>() { "2022-12-04", "", "" };
+        }
+
+        public static List<object> Day05()
+        {
+            return new List<object>() { "2022-12-05", "", "" };
+        }
+
+        public static List<object> Day06()
+        {
+            return new List<object>() { "2022-12-06", "", "" };
+        }
+
+        public static List<object> Day07()
+        {
+            return new List<object>() { "2022-12-07", "", "" };
+        }
+
+        public static List<object> Day08()
+        {
+            return new List<object>() { "2022-12-08", "", "" };
+        }
+
+        public static List<object> Day09()
+        {
+            return new List<object>() { "2022-12-09", "", "" };
+        }
+
+        public static List<object> Day10()
+        {
+            return new List<object>() { "2022-12-10", "", "" };
+        }
+
+        public static List<object> Day11()
+        {
+            return new List<object>() { "2022-12-11", "", "" };
+        }
+
+        public static List<object> Day12()
+        {
+            return new List<object>() { "2022-12-12", "", "" };
+        }
+
+        public static List<object> Day13()
+        {
+            return new List<object>() { "2022-12-13", "", "" };
+        }
+
+        public static List<object> Day14()
+        {
+            return new List<object>() { "2022-12-14", "", "" };
+        }
+
+        public static List<object> Day15()
+        {
+            return new List<object>() { "2022-12-15", "", "" };
+        }
+
+        public static List<object> Day16()
+        {
+            return new List<object>() { "2022-12-16", "", "" };
+        }
+
+        public static List<object> Day17()
+        {
+            return new List<object>() { "2022-12-17", "", "" };
+        }
+
+        public static List<object> Day18()
+        {
+            return new List<object>() { "2022-12-18", "", "" };
+        }
+
+        public static List<object> Day19()
+        {
+            return new List<object>() { "2022-12-19", "", "" };
+        }
+
+        public static List<object> Day20()
+        {
+            return new List<object>() { "2022-12-20", "", "" };
+        }
+
+        public static List<object> Day21()
+        {
+            return new List<object>() { "2022-12-21", "", "" };
+        }
+
+        public static List<object> Day22()
+        {
+            return new List<object>() { "2022-12-22", "", "" };
+        }
+
+        public static List<object> Day23()
+        {
+            return new List<object>() { "2022-12-23", "", "" };
+        }
+
+        public static List<object> Day24()
+        {
+            return new List<object>() { "2022-12-24", "", "" };
+        }
+
     }
 }
--- AokSJ/Program.cs
+++ AokSJ/Program.cs
@@ -3,18 +3,31 @@
 using AokSJ;
 using ConsoleTableExt;
 
-Console.WriteLine("AdventOfCode!");
-Console.WriteLine("");
-
 var columnNames = new List<string>() { "Date", "Part One", "Part Two" };
-var summaries = new List<List<object>>() { DailyQuiz.Day01()};
 var alignments = new Dictionary<int, TextAligntment>
 {
     {1, TextAligntment.Right},
     {2, TextAligntment.Right}
 };
+
+Console.WriteLine("AdventOfCode 2022 | Stefan!");
+Console.WriteLine("");
+
+
+var summaries = new List<List<object>>()
+{
+    DailyQuiz.Day01(), DailyQuiz.Day02(), DailyQuiz.Day03(), DailyQuiz.Day04(),
+    DailyQuiz.Day05(), DailyQuiz.Day06(), DailyQuiz.Day07(), DailyQuiz.Day08(),
+    DailyQuiz.Day09(), DailyQuiz.Day10(), DailyQuiz.Day11(), DailyQuiz.Day12(),
+    DailyQuiz.Day13(), DailyQuiz.Day14(), DailyQuiz.Day15(), DailyQuiz.Day16(),
+    DailyQuiz.Day17(), DailyQuiz.Day18(), DailyQuiz.Day19(), DailyQuiz.Day20(),
+    DailyQuiz.Day21(), DailyQuiz.Day22(), DailyQuiz.Day23(), DailyQuiz.Day24()
+};
+
+var daysToShow = DateTime.Compare(DateTime.Now.Date, DateTime.Parse("2022-12-24").Date) > 0 ? 24 : DateTime.Now.Day;
+
 ConsoleTableBuilder
-    .From(summaries)
+    .From(summaries.Take(daysToShow).ToList())
     .WithColumn(columnNames)
     .WithFormat(ConsoleTableBuilderFormat.Minimal)
     .WithTextAlignment(alignments)
