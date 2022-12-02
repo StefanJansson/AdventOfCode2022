d8c82c8b6ee560faf8692a0db01077fc2f2c3f64
Stefan Jansson <stefan@veum.se>
2022-12-02 11:07:16 +0100

    Day 2

--- AokSJ/DailyQuiz.cs
+++ AokSJ/DailyQuiz.cs
@@ -9,14 +9,66 @@ namespace AokSJ
             var fileData = System.IO.File.ReadAllText(@"..\..\..\Files\2022-12-01.txt");
             var splitFileData = fileData.Split("\n\n");
             var summaries = splitFileData
-                .Select(_ => Regex.Matches(_, @"\d+").Select(_ => Convert.ToInt64(_.Value)).Sum())
-                .OrderByDescending(_ => _).ToList();
-            return new List<object>() { "2022-12-01", summaries.Max(), summaries.Take(3).Sum()};
+                .Select(_ => Regex.Matches(_, @"\d+")
+                .Select(_ => Convert.ToInt64(_.Value)).Sum())
+                .OrderByDescending(_ => _)
+                .ToList();
+            return new List<object>() { "2022-12-01", summaries.First(), summaries.Take(3).Sum() };
         }
 
         public static List<object> Day02()
         {
-            return new List<object>() { "2022-12-02", "", "" };
+            var fileData = System.IO.File.ReadAllLines(@"..\..\..\Files\2022-12-02.txt").ToList();
+
+            var opponentRockCode = "A";
+            var opponentPaperCode = "B";
+            var opponentScissorCode = "C";
+            var myRockCode = "X";
+            var myPaperCode = "Y";
+            var myScissorCode = "Z";
+
+            var opponentOptionCodes = new[] { opponentRockCode, opponentPaperCode, opponentScissorCode };
+            var myOptionCodes = new[] { myRockCode, myPaperCode, myScissorCode };
+            var winningResults = new[] { $"{opponentRockCode} {myPaperCode}", $"{opponentPaperCode} {myScissorCode}", $"{opponentScissorCode} {myRockCode}" };
+            var tieResults = new[] { $"{opponentRockCode} {myRockCode}", $"{opponentPaperCode} {myPaperCode}", $"{opponentScissorCode} {myScissorCode}" };
+
+
+            // Part One
+
+            var choiceBonusesPartOne =
+                fileData.Count(_ => _.EndsWith(myRockCode)) +
+                2 * fileData.Count(_ => _.EndsWith(myPaperCode)) +
+                3 * fileData.Count(_ => _.EndsWith(myScissorCode));
+
+            var wonMatchesPartOne = fileData.Count(_ => winningResults.Contains(_));
+            var tiedMatchesPartOne = fileData.Count(_ => tieResults.Contains(_));
+
+
+            // Part Two
+
+            var looseCode = "X";
+            var tieResultCode = "Y";
+            var winResultCode = "Z";
+            var expectedResults = new[] { tieResultCode, winResultCode, looseCode };
+
+            for (var i = 0; i < fileData.Count; i++)
+            {
+                fileData[i] = fileData[i].Substring(0, 2) +
+                              myOptionCodes[
+                                  (Array.IndexOf(opponentOptionCodes, fileData[i].Substring(0, 1)) +
+                                   Array.IndexOf(expectedResults, fileData[i].Substring(2, 1))) % 3];
+                
+            }
+
+            var wonMatchesPartTwo = fileData.Count(_ => winningResults.Contains(_));
+            var tiedMatchesPartTwo = fileData.Count(_ => tieResults.Contains(_));
+            
+            var choiceBonusesPartTwo =
+                fileData.Count(_ => _.EndsWith(myRockCode)) +
+                2 * fileData.Count(_ => _.EndsWith(myPaperCode)) +
+                3 * fileData.Count(_ => _.EndsWith(myScissorCode));
+
+            return new List<object>() { "2022-12-02", choiceBonusesPartOne + 6 * wonMatchesPartOne + 3 * tiedMatchesPartOne, choiceBonusesPartTwo + 6 * wonMatchesPartTwo + 3 * tiedMatchesPartTwo };
         }
 
         public static List<object> Day03()
